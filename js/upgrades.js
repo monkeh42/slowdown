649252084d34ad13c6d5b32fff2a68a87d0af5a2
monkeh42 <72186713+monkeh42@users.noreply.github.com>
2021-08-10 16:12:24 -0400

    upgrade stuff

---
+++ js/upgrades.js
@@ -0,0 +1,17 @@
+var upgrades = {
+    1: {
+        title: "Unlock wind resistance",
+        cost: new Decimal("5e1005"),
+    }
+}
+
+function canAfford(id) {
+    return player.distance.gte(upgrades[id].cost);
+}
+
+function buyUpgrade(id) {
+    if (!canAfford(id)) { return }
+    player.upgrades.push(id);
+    document.getElementById("upgrade-" + id).classList.remove("cant");
+    document.getElementById("upgrade-" + id).classList.add("bought");
+}
--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var player = {
     force: new Decimal("1e2000"),
     mass: new Decimal("1e1000"),
     speed: new Decimal(0),
+    distance: new Decimal(0),
     lastUpdate: new Date().getTime(),
+
+    upgrades: [],
 }
 
 function init() {
@@ -16,15 +19,32 @@ function startInterval() {
 function gameLoop() {
     var currentUpdate = new Date().getTime();
     diff = new Decimal(currentUpdate - player.lastUpdate);
+    player.distance = player.distance.plus(calculateDistance(player.speed, player.speed.plus(calculateAcc().times(diff.div(1000))), diff))
     player.speed = player.speed.plus(calculateAcc().times(diff.div(1000)));
 
     document.getElementById("speed").innerHTML = regularFormat(player.speed, 2);
+    document.getElementById("distance").innerHTML = regularFormat(player.distance, 2);
     document.getElementById("acc").innerHTML = regularFormat(calculateAcc(), 1);
     document.getElementById("mass").innerHTML = regularFormat(player.mass, 1);
     document.getElementById("force").innerHTML = regularFormat(player.force, 1);
+
+    if (!player.upgrades.includes(id)) {
+        if (canAfford(1) && document.getElementById("upgrade-1").classList.contains("cant")) {
+            document.getElementById("upgrade-1").classList.remove("cant");
+            document.getElementById("upgrade-1").classList.add("can");
+        } else if (!canAfford(1) && document.getElementById("upgrade-1").classList.contains("can")) {
+            document.getElementById("upgrade-1").classList.remove("can");
+            document.getElementById("upgrade-1").classList.add("cant");
+        }
+    }
 }
 
 function calculateAcc() {
     let g = player.force.div(player.mass);
     return g;
+}
+
+function calculateDistance(start, end, diff) {
+    let avg = start.plus(end).div(2);
+    return avg.times(diff.div(1000));
 }
