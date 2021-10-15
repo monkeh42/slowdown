a2ee4b59f6e5741808816e38b7334caa0a795fe7
monkeh42 <72186713+monkeh42@users.noreply.github.com>
2021-10-15 13:33:56 -0400

    struf

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var player = {
     distance: new Decimal(0),
     lastUpdate: new Date().getTime(),
 
+    dragCoeff: new Decimal(0.5),
+    surfaceArea: new Decimal(1),
+    airDensity: new Decimal(1),
+
     upgrades: [],
 }
 
@@ -19,6 +23,7 @@ function startInterval() {
 function gameLoop() {
     var currentUpdate = new Date().getTime();
     diff = new Decimal(currentUpdate - player.lastUpdate);
+    if (player.upgrades.includes(1)) { player.force = player.force.minus(calculateAirRes()); }
     player.distance = player.distance.plus(calculateDistance(player.speed, player.speed.plus(calculateAcc().times(diff.div(1000))), diff))
     player.speed = player.speed.plus(calculateAcc().times(diff.div(1000)));
 
@@ -27,6 +32,8 @@ function gameLoop() {
     document.getElementById("acc").innerHTML = regularFormat(calculateAcc(), 1);
     document.getElementById("mass").innerHTML = regularFormat(player.mass, 1);
     document.getElementById("force").innerHTML = regularFormat(player.force, 1);
+    document.getElementById("air").innerHTML = regularFormat(calculateAirRes(), 1);
+    document.getElementById("newspeed").innerHTML = regularFormat(calculateSpeed(), 1);
 
     if (!player.upgrades.includes(1)) {
         if (canAfford(1) && document.getElementById("upgrade-1").classList.contains("cant")) {
@@ -42,6 +49,16 @@ function gameLoop() {
 function calculateAcc() {
     let g = player.force.div(player.mass);
     return g;
+}
+
+function calculateSpeed() {
+    let v = player.force.times(2).div(player.airDensity.times(player.dragCoeff).times(player.surfaceArea)).sqrt();
+    return v;
+}
+
+function calculateAirRes() {
+    let r = player.airDensity.times(player.dragCoeff).times(player.surfaceArea).div(2).times(Decimal.pow(player.speed, 1.5));
+    return r;
 }
 
 function calculateDistance(start, end, diff) {
